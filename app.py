a9e722fbfd1eb29c488a6f250328f0cb076c3354
Oyindamola Olatunji <ikeadeoyin@gmail.com>
2021-10-18 18:49:59 +0100

    made changes

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ from logging import Formatter, FileHandler, error
 from flask_wtf import Form
 from sqlalchemy.orm import backref
 from forms import *
+from models import *
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -29,97 +30,6 @@ db = SQLAlchemy(app)
 # TODO: connect to a local postgresql database
 migrate = Migrate(app, db)
 
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    genres = db.Column(db.ARRAY(db.String()))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String())
-    shows = db.relationship("Show", backref="venue", cascade="all, delete")
-
-    def __init(self,name, city,  state, address, phone, image_link, facebook_link, genres, seeking_talent, seeking_description, website_link):
-      self.name = name
-      self.city = city
-      self.state = state
-      self.address = address
-      self.phone = phone
-      self.image_link = image_link
-      self.facebook_link = facebook_link
-      self.website_link = website_link
-      self.genres = genres
-      self.seeking_talent = seeking_talent
-      self.seeking_description = seeking_description
-
-    def __repr__(self):
-      return f"<Venue {self.id} {self.name} {self.city} {self.state} {self.genres} {self.website_link} {self.seeking_talent}>"
-
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String(120)))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    # genres = db.Column(db.String())
-    website_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String())
-    shows = db.relationship("Show", backref="artist", cascade="all, delete")
-
-    def __init(self,name, city,  state, phone, image_link, facebook_link, genres, seeking_venue, seeking_description, website_link):
-      self.name = name
-      self.city = city
-      self.state = state
-      self.phone = phone
-      self.image_link = image_link
-      self.facebook_link = facebook_link
-      self.website_link = website_link
-      self.genres = genres
-      self.seeking_venue = seeking_venue
-      self.seeking_description = seeking_description
-
-    def __repr__(self):
-       return f"<Artist {self.id} {self.name} {self.city}>"
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-
-class Show(db.Model):
-  __tablename__ = "Show"
-
-  id = db.Column(db.Integer, primary_key=True)
-  artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"), nullable=False)
-  venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"), nullable=False)
-  start_time = db.Column(db.DateTime, nullable=False)
-
-  def __init__(self,artist_id, venue_id, start_time):
-    self.artist_id = artist_id
-    self.venue_id = venue_id
-    self.start_time = start_time
-
-
-  def __repr__(self):
-      return f"<Show {self.id} {self.artist_id} {self.venue_id}>"
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -160,7 +70,8 @@ def venues():
     venues = Venue.query.filter_by(state=area.state).filter_by(city=area.city).all()
     # print(venues)
     for venue in venues:
-      num_upcoming_shows = Show.query.filter(Show.venue_id==venue.id).filter(Show.start_time > datetime.now()).count()
+      num_upcoming_shows = db.session.query(Show).join(Venue).filter(Show.venue_id==venue.id).filter(Show.start_time > datetime.now()).count()
+      # num_upcoming_shows = Show.query.filter(Show.venue_id==venue.id).filter(Show.start_time > datetime.now()).count()
       #print (num_upcoming_shows)
       venue_list.append({
         "id": venue.id,
