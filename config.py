f16b07b7f685eeaa89ab4782b7b6976a74d90093
Oyindamola Olatunji <ikeadeoyin@gmail.com>
2021-10-18 18:47:28 +0100

    refactored to use env variables

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,8 +8,14 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:boluwatife@localhost:5432/fyyur'
+# SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:boluwatife@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+print(SQLALCHEMY_DATABASE_URI)
